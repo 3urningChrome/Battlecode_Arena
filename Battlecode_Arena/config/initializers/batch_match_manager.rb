7b039962f3187ecb09fa8ae220b6703ff7f5528f
3urningChrome <christop_phillips@hotmail.com>
2015-09-22 16:13:00 +0000

    heroku give you a worker for free. set it to work  full time!

--- Battlecode_Arena/config/initializers/batch_match_manager.rb
+++ Battlecode_Arena/config/initializers/batch_match_manager.rb
@@ -9,17 +9,17 @@ bin_scheduler = Rufus::Scheduler.new
 #  process
 #end
 #
-puts "Rails production? #{Rails.env.production?}"
-if Rails.env.production?
-  puts "Dyno? #{ENV['DYNO']}"
-  if ENV['DYNO'].match(/^web\./)
-    scheduler.every("30s") do
-
-      scheduler.pause
-    %x[heroku run rake batch.process.rake]
-  
+#puts "Rails production? #{Rails.env.production?}"
+#if Rails.env.production?
+#  puts "Dyno? #{ENV['DYNO']}"
+#  if ENV['DYNO'].match(/^web\./)
+#    scheduler.every("30s") do
+#
+#      scheduler.pause
+#    %x[heroku run rake batch.process.rake]
+#  
 #  bin_scheduler.resume if bin_scheduler.paused?
-      scheduler.resume if scheduler.paused?
-    end
-  end
-end
+#      scheduler.resume if scheduler.paused?
+#    end
+#  end
+#end
