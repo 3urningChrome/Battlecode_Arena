52ede8bb5821a723fa895c313c74686c49687bae
3urningChrome <christop_phillips@hotmail.com>
2016-01-15 10:34:45 +0000

    reset wins/losses on resubmission

--- Battlecode_Arena/app/models/competitor.rb
+++ Battlecode_Arena/app/models/competitor.rb
@@ -59,4 +59,12 @@ class Competitor < ActiveRecord::Base
     def get_full_name()
         return "#{self.name}.#{self.submission}"
     end
+    
+    def get_number_of_games_won()
+       return Game.where("(full_name_a = ? = ? OR full_name_b = ? = ?) AND winner = ?", get_full_name(),get_full_name(),competitor_name).count
+    end
+    
+    def get_number_of_games_lost()
+       return Game.where("(full_name_a = ? = ? OR full_name_b = ? = ?) AND winner != ?", get_full_name(),get_full_name(),competitor_name).count
+    end
 end
